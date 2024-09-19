4647bf9ad7541a3858868d1cc877e543e2990b1d
Your Name <you@example.com>
2024-09-18 20:53:17 -0600

    18/09/2024 Leo

---
+++ Script.js
@@ -0,0 +1,25 @@
+/**/ 
+
+//VA
+
+let nombre = "Leo";
+const PI=3.1416;
+var edad=25;
+
+/*Tipos de datos:
+String
+number
+bolean:true, false
+*/
+
+let mensaje="hola mundo";
+let numero =42;
+let estudiante=true;
+
+//Operadores matematicos
+
+let a=5;
+let b=2;
+
+let suma=a+b;
+let resta=a-b;
